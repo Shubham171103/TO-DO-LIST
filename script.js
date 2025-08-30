a4d115d14daba416abdd2aa73945099274628746
Shubham171103 <149925313+Shubham171103@users.noreply.github.com>
2025-08-31 01:18:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const InputBox = document.getElementById("input-box")
+const ListContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(InputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = InputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    InputBox.value='';
+     saveData();
+}
+
+let listContainer = document.getElementById("list-container");
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");   // Toggle checked class
+        saveData();
+    } 
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();        // Remove list item
+        saveData();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
